db52a5689a1118d3f93e1ccdeca7ebab8470cab4
GhostDogGames <dorg50@gmail.com>
2024-10-23 18:23:02 +0300

    Readable code

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,53 @@
-// Function to find and remove all <a> tags with title "Shorts"
+/**
+ * Removes the Shorts section buttons
+ * Works by finding and removing all <a> tags with title "Shorts"
+ */
 function removeShortsLinks() {
-    // Select all <a> elements with title attribute "Shorts"
     const shortsLinks = document.querySelectorAll('a[title="Shorts"]');
+    if (!shortsLinks) {
+        console.log("Can't find links to the Shorts page");
+        return;
+    }
 
     shortsLinks.forEach(link => {
-        // Remove each found link
         link.remove();
         console.log("Removed an <a> tag with title 'Shorts'.");
     });
 }
 
+/**
+ * Removes the Shorts videos shown when viewing a video
+ * Works by removing all html elements of the custom Shorts view
+ */
 function removeShortsVideos() {
-    // Select all <a> elements with title attribute "Shorts"
     const shortsVideos = document.querySelectorAll('ytm-shorts-lockup-view-model-v2');
 
-    shortsVideos.forEach(link => {
+    if (!shortsVideos) {
+        console.log("Can't find Shorts videos");
+        return;
+    }
+
+    shortsVideos.forEach(video => {
         // Remove each found link
-        link.remove();
-        console.log("Removed a short video.");
+        video.remove();
+        console.log("Removed a Shorts video.");
     });
 }
 
+/**
+ * One function that removes all Shorts functionality
+ */
+function removeShortsAll() {
+    removeShortsLinks();
+    removeShortsVideos();
+}
+
 // Run the function when the page is loaded
-window.addEventListener('load', removeShortsLinks);
+window.addEventListener('load', removeShortsAll);
 
 // Observe for dynamically loaded content that might include new <a> tags
 const observer = new MutationObserver(() => {
-    removeShortsLinks();
-    removeShortsVideos();
+    removeShortsAll();
 });
 
 // Observe changes to the body element to catch dynamic content loading
